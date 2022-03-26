3befba2ab6ece1c224b21b80272796363501ccdf
Maya Mangattu <mayamangattu03@gmail.com>
2022-03-26 18:03:49 +0530

    changed

--- signup.js
+++ signup.js
@@ -5,12 +5,13 @@ function validate(){
   let password=document.getElementById('password');
   let cpassword=document.getElementById('cpassword');
   let email=document.getElementById("email");
- 
+ let message=document.getElementById('message');
+ let strength=document.getElementById('strength');
    
     
      let phonenumber=document.getElementById('phonenumber');
      let userformat=/^[A-Za-z0-9_.]{5,20}$/;
-     let passwordformat=  /^(?=.*[0-9])|(?=.*[!@#&%^&*])[a-zA-Z0-9!@#&%^&*]{8,16}$/;
+     let passwordformat=  /^(?=.*[0-9])|(?=.*[!@#&%^&*])[a-zA-Z0-9!@#&%^&*]$/;
      let emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
      let phonenumberformat=/^([0-9]{10})|([0-9]{3}[-.,][0-9]{3}[-.,][0-9]{4})$/;
   
@@ -30,21 +31,39 @@ function validate(){
 
   document.getElementById('passworderror').innerHTML="";
 }else{
-  document.getElementById('passworderror').innerHTML="**Password must contain at least 6 characters and should contain at least one uppercase,lowercase,number and special character";
+  document.getElementById('passworderror').innerHTML="**Password must contain at least 8 characters and should contain at least one uppercase,lowercase,number and special character";
   return false;
 
 }
 
+if(password.value.length>0){
+  
+   message.style.display="block";
+}
+ else{
+  message.style.display="none";
+   return false;
+ }
 
-if(password.value.length<5){
-  document.getElementById('passworderror').innerHTML="**password length must be greater than 6 characters";
+
+if(password.value.length<=4){
+  document.getElementById('strength').innerHTML="password is poor";
+  password.style.borderColor="red";
+  message.style.color="red";
   return false;
 }
-if(password.value.length>25){
-  document.getElementById('passworderror').innerHTML="**password length must be less than 25 characters";
+if(password.value.length>=5&&password.value.length<8){
+  document.getElementById('strength').innerHTML=" password is medium";
+  password.style.borderColor="orange";
+  message.style.color="orange";
   return false;
 }
-
+if(password.value.length>=8){
+  document.getElementById('strength').innerHTML="password is strong";
+  password.style.borderColor="green";
+  message.style.color="green";
+  return false;
+}
 
 
 
